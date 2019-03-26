4fcd7ac09e3c36f9df603536204d74e4ccd863b4
pandyakaa <pandyaka.aptanagi@gmail.com>
2019-03-26 16:40:12 +0700

    BFS added

--- checkways.py
+++ checkways.py
@@ -3,11 +3,11 @@ def inputMaze(filename) :
     f = open("{}.txt".format(filename),"r")
     for line in f :
         arr.append([int(c) for c in line.strip()])
-    panjang = len(arr[0])
-    lebar = len(arr)
+    baris = len(arr)
+    kolom = len(arr[0])
 
     f.close()
-    return arr,panjang,lebar
+    return arr,baris,kolom
 
 def printMaze(m) :
     for i in m :
@@ -21,55 +21,50 @@ def printMaze(m) :
 def printSolution(m) :
     for i in m :
         for j in i :
-            if (j == 0 ) : 
+            if (j == 1 ) : 
+                print("# ",end = '')
+            elif (j == 3 or j == 2) :
                 print(" ",end = '')
             else :
-                print("# ",end = '')
+                print("  ", end = '')
         print()
 
-def isSafe(m,x,y) :
-    if ( x >= 0 and x < len(m[0]) and y >= 0 and y < len(m) and m[x][y] == 0 ) :
+# Solusi menggunakan Algoritma BFS
+def BFS(maze,x,y) :
+    if (maze[x][y] == 2) :
         return True
-    else :
-        return False
-
-def BFSUtil(m,x,y,sol) :
-    if ( x == len(m[0]) - 1 and y == len(m) - 1 ) :
-        sol[x][y] = 0
-        return True
-    
-    if isSafe(m,x,y) == True :
-        sol[x][y] = 0
-
-        if BFSUtil(m,x+1,y,sol) == True :
-            return True
-        
-        if BFSUtil(m,x,y+1,sol) == True :
-            return True
-        
-        sol[x][y] = 1
-        return False
-
-def BFS(m,x,y) :
-
-    sol = [ [ 1 for j in range(len(m[0])) ] for i in range(len(m)) ] 
-      
-    if BFSUtil(m, x, y, sol) == False: 
-        print("Solution doesn't exist"); 
-        return False
-      
-    printSolution(sol) 
-    return True
+    elif (maze[x][y] == 0 ) :
+        maze[x][y] = 3
+        if(x < len(maze) -1 ) :
+            if BFS(maze,x+1,y) :
+                return True
+        if(x > 0) :
+            if BFS(maze,x-1,y) :
+                return True
+        if ( y < len(maze[x])-1 ) :
+            if BFS(maze,x,y+1) :
+                return True
+        if ( y > 0) :
+            if BFS(maze,x,y-1) :
+                return True
 
 if __name__ == "__main__":
     file = input("Masukkan nama file : ")
-    maze, size_x , size_y = inputMaze(file)
-    
+    maze, baris , kolom = inputMaze(file)
+
+    # Finish ada pada angka 2
+    for i in maze :
+        print(i)
+    print()
     printMaze(maze)
     print()
-    printSolution(maze)
-    print()
-    BFS(maze,0,1)
+    if (BFS(maze,0,1)) :
+        printSolution(maze)
+    else :
+        print("NOT FOUND")
+    
+    
+
     
 
     
